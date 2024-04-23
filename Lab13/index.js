3066627d98086e2d8b6348ab8069654fc37cea60
Mike <mike@MacBook-Pro-de-Mike.local>
2024-04-23 14:58:07 -0600

    Add: Lab13

---
+++ Lab13/index.js
@@ -0,0 +1,30 @@
+const http    = require('http');
+const express = require('express');
+const path    = require('path');
+const fs      = require('fs');
+const app     = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response, next) => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.send("Hola Mundo");
+    response.end(); 
+});
+
+app.get("/test_ejs",(request,response,next)=>{
+    response.render("usuarios/login");
+});
+
+const rutasUsuarios = require('./routes/usuarios.routes');
+app.use('/usuarios', rutasUsuarios);
+
+const server = http.createServer( (request, response) => {    
+    console.log(request.url);
+});
+app.listen(3002);
---
+++ Lab13/models/usuarios.model.js
@@ -0,0 +1,7 @@
+exports.login = function(correo, constrasena){
+    return{
+        nombre: "Samuel",
+        id: 1,
+        activo:true
+    };
+}
